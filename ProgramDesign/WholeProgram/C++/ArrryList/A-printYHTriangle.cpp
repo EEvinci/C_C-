b933e04e60ef55093764c2a9639001a2ca3eb2f4
Evinci <93924805+EEvinci@users.noreply.github.com>
2022-11-23 22:49:34 +0800

    fucking code

--- ProgramDesign/WholeProgram/C++/ArrryList/A-printYHTriangle.cpp
+++ ProgramDesign/WholeProgram/C++/ArrryList/A-printYHTriangle.cpp
@@ -1,14 +1,16 @@
 #include <bits/stdc++.h>
 using namespace std;
+
+int a[15][15];
+
 int main()
 {
     int n;
     cin >> n;
-    int a[n][n];
     // first, fill in the first colum and matrix diagnal elements
     for (int i = 0; i < n; i++)
     {
-        a[0][i] = a[i][i] = 1;
+        a[i][0] = a[i][i] = 1;
     }
     // second, calculate the other elements based on existing elements
     // in this step, we should analysis it by its data distribusion chart
@@ -17,21 +19,30 @@ int main()
     for (int i = 2; i < n; i++)
     {
         // and the column loop variable need to start from 1
-        // you need to control the upper bound of the variable j
+        // you 5need to control the upper bound of the variable j
         // its value can not be greater than i
         // it aims to limit the count of number in one row
-       for(int j = 1; j < i; j++){
-            a[i][j] = a[i-1][j-1] + a[i-1][j];
-       }
+        for (int j = 1; j < i; j++)
+        {
+            a[i][j] = a[i - 1][j - 1] + a[i - 1][j];
+        }
     }
 
     // third,you can print the result in this step
-    for(int i = 0; i < n; i++){
+    for (int i = 0; i < n; i++)
+    {
         // you need to control the blanks firstly
-        for(int j = 1; j < n-i; j++){
-            
+        for (int j = 1; j < n - i; j++) // this operation is <, because i==0 in the outside loop,fuck
+        {
+            cout << " ";
         }
+        for (int k = 0; k <= i; k++)
+        {
+            printf("%4d", a[i][k]);
+        }
+        cout << "\n";
     }
+
     system("pause");
     return 0;
 }
